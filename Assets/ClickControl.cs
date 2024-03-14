8f90eb7e139892c467d3f1aaf31ef467c489b4d7
JRDrazypoffs <dragon.jp49@gmail.com>
2024-03-14 23:12:38 +0800

    Fixed Issue with HOG element not working
    Use Sprite Renderer instead of Image Renderer, moved camera and resized everthing. Sprite renderer uses the negatives layer in that tutorial lol

--- Assets/ClickControl.cs
+++ Assets/ClickControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClickControl : MonoBehaviour
 {
@@ -16,12 +17,25 @@ public class ClickControl : MonoBehaviour
     // // Update is called once per frame
     // void Update()
     // {}
-
     private void OnMouseDown(){
+        // int HiddenObjectCount = 0;
         ObjectName = gameObject.name;
-        UnityEngine.Debug.Log (ObjectName);
+        Debug.Log (ObjectName);
         Destroy (gameObject);
         Destroy (ObjectNameText);
+        // HiddenObjectCount++;
+
+        // // If found all 10 objects
+        // if(HiddenObjectCount==10){
+        //     UnlockNewArea();
+        // }
     }
-    
+
+    // public void UnlockNewArea(){
+    //     if(SceneManager.GetActiveScene().buildIndex <= PlayerPrefs.GetInt("ReachedIndex")){
+    //         PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1 );
+    //         PlayerPrefs.SetInt("UnlockedArea", PlayerPrefs.GetInt("UnlockedArea", 1) + 1 );
+    //         PlayerPrefs.Save();
+    //     }
+    // }
 }
