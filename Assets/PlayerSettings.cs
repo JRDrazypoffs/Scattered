53e6d414617b19f8b93b574dd2e2e777bb1d34f1
JRDrazypoffs <dragon.jp49@gmail.com>
2024-03-15 22:17:37 +0800

    Altered Player settings
    from brackeys setting tutorial mostly working
    
    next figure how to save and set as default values from the inputs

--- Assets/PlayerSettings.cs
+++ Assets/PlayerSettings.cs
@@ -6,16 +6,23 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEditor;
+using TMPro;
 
 public class PlayerSettings : MonoBehaviour
-{
+{   
+
+    [SerializeField] TMP_InputField UsernameInput;
+    public TMP_Dropdown difficultyDropdown;
+    public TMP_Dropdown graphicsDropdown;
+    public TMP_Dropdown resolutionDropdown;
     public AudioMixer MasterAudioMixer;
 
-    public Dropdown resolutionDropdown;
     Resolution[] resolutions;
 
     // Start is called before the first frame update
     void Start(){
+
+        // Resolution Setting
         resolutions = Screen.resolutions;
 
         // clear placeholders in resolution dropdown
@@ -23,17 +30,40 @@ public class PlayerSettings : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
+
+        // Processing the resolutions obtained fromd evice into arrays to display as dropdown options.
         for(int i = 0; i < resolutions.Length; i++){
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && 
+            resolutions[i].height == Screen.currentResolution.height){
+                currentResolutionIndex = i;
+            }
         }
 
+        // Set Default resolution as current resolution
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     // // Update is called once per frame
     // void Update(){
     // }
+
+    public void GetUserSettings(){
+        // TODO:
+        // Get user settings
+        // save user settings to player prefs
+    }
+
+    public void SetUserSettings(){
+        // TODO:
+        // Get user settings from player prefs
+        // set user settings as default value in inputs
+    }
 
     public void SetMasterVolume(float MasterVolume){
         // Debug.Log(MasterVolume);
@@ -41,6 +71,7 @@ public class PlayerSettings : MonoBehaviour
     }
 
     // TODO: Set Separate Volumes for BGM and SFX
+
     // public void SetBGMVolume(float BGMVolume){
     //     // Debug.Log(MasterVolume);
     //     MasterAudioMixer.SetFloat("BGMVolume", BGMVolume);
@@ -59,5 +90,9 @@ public class PlayerSettings : MonoBehaviour
         Screen.fullScreen = IsFullscreen;
     }
 
+    public void SetResolution(int resolutionIndex){
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
 }
