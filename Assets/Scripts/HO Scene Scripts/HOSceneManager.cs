a849a18bbdbdd9f4c8ebe7470d31b44fccc27800
JRDrazypoffs <dragon.jp49@gmail.com>
2024-03-17 23:29:58 +0800

    commented some code

--- Assets/Scripts/HO Scene Scripts/HOSceneManager.cs
+++ Assets/Scripts/HO Scene Scripts/HOSceneManager.cs
@@ -9,9 +9,9 @@ using UnityEngine.UI;
 public class SceneMenu : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start(){
-    }
+    // // Start is called before the first frame update
+    // void Start(){
+    // }
 
     // // Update is called once per frame
     // void Update(){
