0157e51a2595dc334b7b2d128bb4cb0ae4fd1cf1
JRDrazypoffs <dragon.jp49@gmail.com>
2024-03-17 23:09:01 +0800

    Updated Scene Clear Panels
    -pop animation for clues
    -sfx updates for buttons
    -environment sfx for cozy feel

--- Assets/Scripts/HO Scene Scripts/HOSceneManager.cs
+++ Assets/Scripts/HO Scene Scripts/HOSceneManager.cs
@@ -8,9 +8,10 @@ using UnityEngine.UI;
 
 public class SceneMenu : MonoBehaviour
 {
+
     // Start is called before the first frame update
-    // void Start(){   
-    // }
+    void Start(){
+    }
 
     // // Update is called once per frame
     // void Update(){
