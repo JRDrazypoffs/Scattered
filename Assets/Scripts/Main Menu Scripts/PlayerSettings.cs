58b60ddf60768197882dad29260f13470eaace48
JRDrazypoffs <dragon.jp49@gmail.com>
2024-03-19 12:03:48 +0800

    Some Set Player Settings update
    -Can save settings for first time
    -doesnt plug in the saved values into the inputs except text
    -value is fixed with saved value
    -need to figure out how to change the selected value of sliders, dropdown and toggle

--- Assets/Scripts/Main Menu Scripts/PlayerSettings.cs
+++ Assets/Scripts/Main Menu Scripts/PlayerSettings.cs
@@ -8,42 +8,42 @@ using UnityEngine.UI;
 using UnityEditor;
 using TMPro;
 using System;
+using Unity.Mathematics;
 
 public class PlayerSettings : MonoBehaviour
 {   
 
     [SerializeField] TMP_InputField UsernameInput;
+    public Slider masterSlider;
+    public Slider bgmSlider;
+    public Slider sfxSlider;
     public TMP_Dropdown difficultyDropdown;
     public TMP_Dropdown graphicsDropdown;
     public TMP_Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle;
+
     public AudioMixer MasterAudioMixer;
 
     Resolution[] resolutions;
 
-    private float MasterVolume;
-    float BGMVolume;
-    float SFXVolume;
-    string Username;
-    int DifficultyIndex;
-    int QualityIndex;
-    int resolutionIndex;
+    private float TempMasterVolume;
+    private float TempBGMVolume;
+    private float TempSFXVolume;
+    private int TempDifficultyIndex;
+    private int TempQualityIndex;
+    private int TempResolutionIndex;
+    private int TempIsFullscreen;
 
+    // currently will save settings if player never input before
+    // but if want to update settings the value is fixed to prev settings
+    // not working as intended maybe need to change the .value parameter to stg else.
 
     // Start is called before the first frame update
     void Start(){
         // check if user has set settings
-        if(
-            PlayerPrefs.HasKey("Player Pref Master Volume") &&
-            PlayerPrefs.HasKey("Player Pref BGM Volume") &&
-            PlayerPrefs.HasKey("Player Pref SFX Volume") &&
-            PlayerPrefs.HasKey("Player Pref Username") &&
-            PlayerPrefs.HasKey("Player Pref Difficulty") &&
-            PlayerPrefs.HasKey("Player Pref Graphic QI") &&
-            PlayerPrefs.HasKey("Player Pref Resolution Index") &&
-            PlayerPrefs.HasKey("Player Pref IsFullscreen") 
-        ){
+        if(PlayerPrefs.HasKey("Settings Has Set")){
             Debug.Log("you have all settings data stored");
-            LoadPrefSettings();
+            LoadData();
         }else{
             Debug.Log("please set your settings");
         }
@@ -79,80 +79,125 @@ public class PlayerSettings : MonoBehaviour
     // void Update(){
     // }
 
-    public void GetUserSettings(){
-        // TODO:
-        // Get user settings
-        // save user settings to player prefs
-    }
-
     public void SetMasterVolume(float MasterVolume){
-        // Debug.Log(MasterVolume);
-        MasterAudioMixer.SetFloat("MasterVolume", MasterVolume);
-        PlayerPrefs.SetFloat("Player Pref Master Volume",MasterVolume);
-        PlayerPrefs.Save();
+        if (PlayerPrefs.HasKey("Settings Has Set")){
+            masterSlider.value = TempMasterVolume;
+            // MasterAudioMixer.SetFloat("MasterVolume", TempMasterVolume);
+        }else{
+            MasterAudioMixer.SetFloat("MasterVolume", MasterVolume);
+            TempMasterVolume = MasterVolume;
+        }
     }
 
     public void SetBGMVolume(float BGMVolume){
-        // Debug.Log(MasterVolume);
-        MasterAudioMixer.SetFloat("BGMVolume", BGMVolume);
-        PlayerPrefs.SetFloat("Player Pref BGM Volume",BGMVolume);
+        if (PlayerPrefs.HasKey("Settings Has Set")){
+            bgmSlider.value = TempBGMVolume;
+            // MasterAudioMixer.SetFloat("BGMVolume", TempBGMVolume);
+        }else{
+            MasterAudioMixer.SetFloat("BGMVolume", BGMVolume);
+            TempBGMVolume = BGMVolume;
+        }
     }
 
     public void SetSFXVolume(float SFXVolume){
-        // Debug.Log(MasterVolume);
-        MasterAudioMixer.SetFloat("SFXVolume", SFXVolume);
-        PlayerPrefs.SetFloat("Player Pref SFX Volume",SFXVolume);
-    }
-
-    public void SetUsername(string UsernameInput){
-        PlayerPrefs.SetString("Player Pref Username",UsernameInput);
+        if (PlayerPrefs.HasKey("Settings Has Set")){
+            sfxSlider.value = TempSFXVolume;
+            // MasterAudioMixer.SetFloat("SFXVolume", TempSFXVolume);
+        }else{
+            MasterAudioMixer.SetFloat("SFXVolume", SFXVolume);
+            TempSFXVolume = SFXVolume;
+        }
     }
 
     public void SetDifficulty(int DifficultyIndex){
-        PlayerPrefs.SetInt("Player Pref Difficulty",DifficultyIndex);
+        if (PlayerPrefs.HasKey("Settings Has Set")){
+            difficultyDropdown.value = TempDifficultyIndex;
+        }else{
+            TempDifficultyIndex = DifficultyIndex;
+        }
     }
 
     public void SetGraphicsQuality(int QualityIndex){
-        QualitySettings.SetQualityLevel(QualityIndex);
-        PlayerPrefs.SetInt("Player Pref Graphic QI",QualityIndex);
+        if (PlayerPrefs.HasKey("Settings Has Set")){
+            graphicsDropdown.value = TempQualityIndex;
+        }else{
+            QualitySettings.SetQualityLevel(QualityIndex);
+            TempQualityIndex = QualityIndex;
+        }
     }
 
     public void SetResolution(int resolutionIndex){
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-        PlayerPrefs.SetInt("Player Pref Resolution Index",resolutionIndex);
+        if (PlayerPrefs.HasKey("Settings Has Set")){
+            resolutionDropdown.value = TempResolutionIndex;
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }else{
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+            TempResolutionIndex = resolutionIndex;
+        }
     }
 
     public void SetFullscreen(bool IsFullscreen){
-        int IntIsFullscreen = 1;
-        Screen.fullScreen = IsFullscreen;
-        // convert bool to int cause playerpref cannot set int
-        if(IsFullscreen==true){
-            IntIsFullscreen = 1;
+        if (PlayerPrefs.HasKey("Settings Has Set")){
+            if(TempIsFullscreen == 1){
+                Screen.fullScreen = true;
+            }else{
+                Screen.fullScreen = false;
+            }
         }else{
-            IntIsFullscreen = 0;
+            Screen.fullScreen = IsFullscreen;
+            // convert bool to int cause playerpref cannot set int
+            if(IsFullscreen==true){
+                TempIsFullscreen = 1;
+            }else{
+                TempIsFullscreen = 0;
+            }
         }
-        PlayerPrefs.SetInt("Player Pref IsFullscreen",IntIsFullscreen);
     }
 
-    private void LoadPrefSettings(){
-        float MasterVolume  = PlayerPrefs.GetFloat("Player Pref Master Volume");
-        float BGMVolume     = PlayerPrefs.GetFloat("Player Pref BGM Volume");
-        float SFXVolume     = PlayerPrefs.GetFloat("Player Pref SFX Volume");
-        string Username     = PlayerPrefs.GetString("Player Pref Username");
-        int DifficultyIndex = PlayerPrefs.GetInt("Player Pref Difficulty");
-        int GraphicQIndex   = PlayerPrefs.GetInt("Player Pref Graphic QI");
-        int ResolutionIndex = PlayerPrefs.GetInt("Player Pref Resolution Index");
-        int IntIsFullscreen = PlayerPrefs.GetInt("Player Pref IsFullscreen");
+    public void SaveData(){
+        PlayerPrefs.SetString("Player Pref Username",UsernameInput.text);
+        PlayerPrefs.SetFloat("Player Pref Master Volume",TempMasterVolume);
+        PlayerPrefs.SetFloat("Player Pref BGM Volume",TempBGMVolume);
+        PlayerPrefs.SetFloat("Player Pref SFX Volume",TempSFXVolume);
+        PlayerPrefs.SetInt("Player Pref Difficulty",TempDifficultyIndex);
+        PlayerPrefs.SetInt("Player Pref Graphic QI",TempQualityIndex);
+        PlayerPrefs.SetInt("Player Pref Resolution Index",TempResolutionIndex);
+        PlayerPrefs.SetInt("Player Pref IsFullscreen",TempIsFullscreen);
+        PlayerPrefs.SetInt("Settings Has Set",1);
+        PlayerPrefs.Save();
 
-        Debug.Log(MasterVolume);
-        Debug.Log(BGMVolume);
-        Debug.Log(SFXVolume);
-        Debug.Log(Username);
-        Debug.Log(DifficultyIndex);
-        Debug.Log(GraphicQIndex);
-        Debug.Log(ResolutionIndex);
-        Debug.Log(IntIsFullscreen);
+        // Initialise all temp variables after save
+        TempMasterVolume = 0;
+        TempBGMVolume = 0;
+        TempSFXVolume = 0;
+        TempDifficultyIndex = 0;
+        TempQualityIndex = 0;
+        TempResolutionIndex = 0;
+        TempIsFullscreen = 0;
+    }
+
+    private void LoadData(){
+        string Username     = PlayerPrefs.GetString("Player Pref Username");
+        UsernameInput.text = Username;
+
+        TempMasterVolume    = PlayerPrefs.GetFloat("Player Pref Master Volume");
+        TempBGMVolume       = PlayerPrefs.GetFloat("Player Pref BGM Volume");
+        TempSFXVolume       = PlayerPrefs.GetFloat("Player Pref SFX Volume");
+        TempDifficultyIndex = PlayerPrefs.GetInt("Player Pref Difficulty");
+        TempQualityIndex    = PlayerPrefs.GetInt("Player Pref Graphic QI");
+        TempResolutionIndex = PlayerPrefs.GetInt("Player Pref Resolution Index");
+        TempIsFullscreen    = PlayerPrefs.GetInt("Player Pref IsFullscreen");
+
+        Debug.Log("Master Vol: "+TempMasterVolume);
+        Debug.Log("BGM Vol: "+TempBGMVolume);
+        Debug.Log("SFX Vol: "+TempSFXVolume);
+        Debug.Log("Username: "+Username);
+        Debug.Log("Difficulty Index: "+TempDifficultyIndex);
+        Debug.Log("Graphic Index: "+TempQualityIndex);
+        Debug.Log("Resolution Index: "+TempResolutionIndex);
+        Debug.Log("Fullscreen: "+TempIsFullscreen);
 
     }
 
