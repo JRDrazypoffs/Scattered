45fd5dfa9f4d760bb0e109b1a6366efc583b82ef
JRDrazypoffs <dragon.jp49@gmail.com>
2024-04-04 23:54:08 +0800

    minor update
    - checks if player has cleared the area already if not play dialogue otherwise enter the hog scene.

--- Assets/Scripts/Map Scripts/SelectArea.cs
+++ Assets/Scripts/Map Scripts/SelectArea.cs
@@ -10,7 +10,7 @@ public class SelectArea : MonoBehaviour
     // public GameObject[] Area;
     public static int currentLevel;
     public static int unlockedLevels;
-
+    
     private void Awake(){
         unlockedLevels = PlayerPrefs.GetInt("Unlocked Levels",0);
 
@@ -23,5 +23,15 @@ public class SelectArea : MonoBehaviour
 
     public void EnterArea(string AreaName){
         SceneManager.LoadScene(AreaName);
+    }
+
+    // please check if this works, generally if current level is higher than unlocked levels 
+    // it means player has already cleared the level before and no need to show dialogue again
+    public void EnterArea(string AreaName, string AreaDialogName){
+        if(currentLevel>unlockedLevels){
+            SceneManager.LoadScene(AreaName);
+        }else{
+            SceneManager.LoadScene(AreaDialogName);
+        }
     }
 }
