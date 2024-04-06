d61ec0120e154f7efb88521112d1624e2ce47a49
JRDrazypoffs <dragon.jp49@gmail.com>
2024-04-06 18:13:49 +0800

    cleanup unused lines or empty scripts

--- Assets/Scripts/Dialog Scripts/DialogAnimatorTooltips.cs
+++ Assets/Scripts/Dialog Scripts/DialogAnimatorTooltips.cs
@@ -13,16 +13,6 @@ public class DialogAnimatorTooltips : MonoBehaviour
     private static int index;
     
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private DialogueVertexAnimator dialogueVertexAnimator;
 
     public void AnimateTooltipsText(){
