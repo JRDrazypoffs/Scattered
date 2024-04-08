fd3658f9aa36de11953cc1ab4f44a38313d3d596
JRDrazypoffs <dragon.jp49@gmail.com>
2024-04-08 10:30:09 +0800

    Main Menu BG Update
    when player havent finish clearing the game, show search bg, else show hug bg

---
+++ Assets/Scripts/Main Menu Scripts/BGChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGChanger : MonoBehaviour
+{
+    public GameObject SearchBG;
+    public GameObject HugBG;
+
+    private int CurrentLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentLevel = PlayerPrefs.GetInt("Reached Index");
+        ChangeBG();
+    }
+
+    void ChangeBG(){
+        if(CurrentLevel >= 9 ){
+            SearchBG.SetActive(false);
+            HugBG.SetActive(true);
+        }else{
+            SearchBG.SetActive(true);
+            HugBG.SetActive(false);
+        }
+    }
+}
